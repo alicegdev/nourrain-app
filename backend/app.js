4719bd1964bd9860969513fc01254524cdd8ca00
gaburaisu <56201393+GaburaisuVGC@users.noreply.github.com>
2024-03-13 14:51:47 +0100

    changes on crud

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('../routes/User.js');
 const mysql = require('mysql');
 
+const app = express();
+
+// Configurer la connexion à la base de données
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'nourrain-dev',
@@ -7,7 +13,22 @@ const connection = mysql.createConnection({
   database: 'nourrain_db'
 });
 
+// Middleware pour analyser le corps des requêtes au format JSON
+app.use(bodyParser.json());
+
+// Connecter à la base de données
 connection.connect((err) => {
   if (err) throw err;
   console.log('Connecté à la base de données MariaDB');
+});
+
+// Middleware pour gérer les routes des utilisateurs
+app.use('/user', userRoutes);
+
+// Port d'écoute du serveur
+const PORT = process.env.PORT || 3000;
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
 });
---
+++ backend/routes/Nourrain.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const Crud = require('../utils/Crud.js');
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
+
+// Modèle User
+const USER_TABLE = 'User';
+
+
+
+module.exports = router;
